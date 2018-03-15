0d39cc29710ecbdfc66f6dfeda239eb927eb5690
Jonah Meijers <jonahmeijers97@gmail.com>
2018-03-15 12:14:44 +0100

    add account router

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 
+const account = require('./routes/account')
+
 const app = express()
-app
+module.exports = app
     .set('view engine', 'ejs')
     .set('views', 'views')
     .use(express.static('assets'))
+    .use('/account', account)
     .get('/', (req, res) => res.render('onboarding.ejs'))
     .listen(1337)
