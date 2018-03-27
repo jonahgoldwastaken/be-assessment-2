1ad7d0bb07abb2bfdda3b44ed81e407165892b98
Jonah Meijers <jonahmeijers97@gmail.com>
2018-03-27 19:52:47 +0200

    change deploy folder

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,7 +4,7 @@ set -x
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v8.9.3/bin
 
-cd /var/www/YOUR-DOMAIN.com && \
+cd /home/deploy/hdate && \
 tar zxvf package.tgz -C . && \
 mv build/package.json . && \
 npm install && \
