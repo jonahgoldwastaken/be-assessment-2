b9636e831b3b8b6eae242fb0f21e6f5024fc0cb8
Jonah Meijers <jonahmeijers97@gmail.com>
2018-03-27 23:48:50 +0200

    some more bugfixing

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,8 +4,8 @@ set -x
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v8.9.3/bin
 
-cd ~/hdate && \
+cd ~/ && \
 tar zxvf package.tgz -C . && \
-mv build/package.json . && \
+cd hdate . && \
 npm install && \
 npm run start
