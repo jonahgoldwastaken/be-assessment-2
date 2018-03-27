10c4846c5e74953db3a0e783db667d7f611126c1
Jonah Meijers <jonahmeijers97@gmail.com>
2018-03-27 23:06:32 +0200

    add ls to test folder naming

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -x
 
+ls .
 tar czf package.tgz build && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
