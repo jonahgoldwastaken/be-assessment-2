b6443e74f616f7a7af62fff49a7a3b2bc6389eeb
Jonah Meijers <jonahmeijers97@gmail.com>
2018-03-27 22:25:24 +0200

    another test

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,7 +4,7 @@ set -x
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v8.9.3/bin
 
-cd /home/deploy/hdate && \
+cd ~/hdate && \
 tar zxvf package.tgz -C . && \
 mv build/package.json . && \
 npm install && \
