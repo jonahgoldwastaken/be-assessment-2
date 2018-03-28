b82c95028dc62a8d250fd090db225c65dc953901
Jonah Meijers <jonahmeijers97@gmail.com>
2018-03-28 21:28:21 +0200

    ~travis~ -> cdd

--- scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -x
-cd ..
-tar czf package.tgz hdate && \
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./hdate/scripts/untar.sh
