2931a7cd8fd654ba1707775c1af7e02f4ea2d0cf
Jonah Meijers <jonahmeijers97@gmail.com>
2018-04-08 01:09:17 +0200

    add base for matching system

--- routes/home.js
+++ routes/home.js
@@ -25,6 +25,59 @@ const homePage = async (req, res, next) => {
     }
 }
 
+/**
+ * Lieks a user, and creates if match if idem
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Function} next
+ */
+const likeUser = async (req, res, next) => {
+    if (account.currentUser.isLoggedIn(req)) {
+        const { id } = req.params
+        try {
+            const loggedInUser = await account.currentUser.get(req)
+            loggedInUser.likes.push(id)
+            await loggedInUser.update(loggedInUser)
+
+            if (account.checkMatch(loggedInUser, id)) {
+                account.process.match(loggedInUser, id)
+                res.redirect('/home')
+            } else {
+                res.redirect('/home')
+            }
+
+        } catch (err) {
+            next(err)
+        }
+    } else {
+        res.redirect('/account/login')
+    }
+}
+
+/**
+ * Dislikes a user, adding its ID to the Account documetn
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Function} next
+ */
+const dislikeUser = async (req, res, next) => {
+    if (account.currentUser.isLoggedIn(req)) {
+        const { id } = req.params
+        try {
+            const loggedInUser = await account.currentUser.get(req)
+            loggedInUser.dislikes.push(id)
+            await loggedInUser.update(loggedInUser)
+            res.redirect('/home')
+        } catch (err) {
+            next(err)
+        }
+    } else {
+        res.redirect('/home')
+    }
+}
+
 module.exports = router
     .get('/', homePage)
+    .post('/like/:id', likeUser)
+    .post('/dislike/:id', dislikeUser)
 
